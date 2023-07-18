c07eb7d343ce5a7a7bdbc1d060b927d7fe0b015a
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-07-19 05:09:17 +0700

    add method constructor

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -10,7 +10,7 @@ import (
 type Repository interface {
 	Get(ctx context.Context, userID int) (*entity.User, error)
 	Create(ctx context.Context, user *UserCreateRequest) (*UserCreateResponse, error)
-	Update(ctx context.Context, user UserUpdateRequest) (*UserUpdateResponse, error)
+	Update(ctx context.Context, user *UserUpdateRequest) (*UserUpdateResponse, error)
 	Delete(ctx context.Context, userID int) error
 }
 
@@ -18,7 +18,13 @@ type UserRepository struct {
 	DB *pgxpool.Pool
 }
 
-func (ur *UserRepository) Get(ctx context.Context, userID int) (*entity.User, error) {
+func NewUserRepository(db *pgxpool.Pool) Repository {
+	return UserRepository{
+		DB: db,
+	}
+}
+
+func (ur UserRepository) Get(ctx context.Context, userID int) (*entity.User, error) {
 	r := ur.DB.QueryRow(ctx, "SELECT id, email, username, password, profile_picture FROM users WHERE id = $1", userID)
 	user := &entity.User{}
 
@@ -29,15 +35,7 @@ func (ur *UserRepository) Get(ctx context.Context, userID int) (*entity.User, er
 	return user, nil
 }
 
-func (ur *UserRepository) Delete(ctx context.Context, userID int) error {
-	_, err := ur.DB.Exec(ctx, "DELETE FROM users WHERE id = $i", userID)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (ur *UserRepository) Create(ctx context.Context, user *UserCreateRequest) (*UserCreateResponse, error) {
+func (ur UserRepository) Create(ctx context.Context, user *UserCreateRequest) (*UserCreateResponse, error) {
 	_, err := ur.DB.Exec(ctx, "INSERT INTO users (email, username, password, profile_picture) VALUES ($1, $2, $3, $4)", user.Email, user.Username, user.Password, user.ProfilePicture)
 	if err != nil {
 		return nil, err
@@ -53,7 +51,7 @@ func (ur *UserRepository) Create(ctx context.Context, user *UserCreateRequest) (
 	return userResponse, nil
 }
 
-func (ur *UserRepository) Update(ctx context.Context, user *UserUpdateRequest) (*UserUpdateResponse, error) {
+func (ur UserRepository) Update(ctx context.Context, user *UserUpdateRequest) (*UserUpdateResponse, error) {
 	SQL := "UPDATE users SET email = $1, username = $2, password = $3, profile_picture = $4 WHERE id = $5"
 	_, err := ur.DB.Exec(ctx, SQL, user.Email, user.Username, user.Password, user.ProfilePicture, user.ID)
 	if err != nil {
@@ -67,4 +65,12 @@ func (ur *UserRepository) Update(ctx context.Context, user *UserUpdateRequest) (
 	userResponse.ProfilePicture = user.ProfilePicture
 
 	return userResponse, nil
+}
+
+func (ur UserRepository) Delete(ctx context.Context, userID int) error {
+	_, err := ur.DB.Exec(ctx, "DELETE FROM users WHERE id = $i", userID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
