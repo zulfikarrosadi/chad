edaa91129f8687eda7f65c0602fefcd7dd74f375
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-07-24 19:51:30 +0700

    initialize all user package and echo so this app can running

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/zulfikarrosadi/chad/internal/user"
+	"github.com/zulfikarrosadi/chad/pkg/db"
+)
 
+func main() {
+	e := echo.New()
+	userRepository := user.NewUserRepository(db.GetConnection())
+	userService := user.NewUserService(userRepository.(user.UserRepositoryImpl))
+	userHandler := user.NewUserAPI(userService.(*user.UserServiceImpl))
+
+	e.POST("/api/users", userHandler.Create)
+	e.GET("/api/users/:userID", userHandler.Get)
+	e.PUT("/api/users/:userID", userHandler.Update)
+	e.DELETE("/api/users/:userID", userHandler.Delete)
+
+	e.Logger.Fatal(e.Start("localhost:3000"))
 }
