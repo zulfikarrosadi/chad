4948246de220abea1ff8dd934e14baaa81c224c1
zulfikarrosadi <zulfikarrosadi31@gmail.com>
2023-07-24 19:55:29 +0700

    rename the interface to UserRepository and struct to UserRepositoryImpl for consistency in whole project
    replace pgx package with default sql from go

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -2,30 +2,33 @@ package user
 
 import (
 	"context"
+	"database/sql"
+	"errors"
+	"log"
 
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/lib/pq"
 	"github.com/zulfikarrosadi/chad/internal/entity"
 )
 
-type Repository interface {
+type UserRepository interface {
 	Get(ctx context.Context, userID int) (*entity.User, error)
 	Create(ctx context.Context, user *UserCreateRequest) (*UserCreateResponse, error)
 	Update(ctx context.Context, user *UserUpdateRequest) (*UserUpdateResponse, error)
 	Delete(ctx context.Context, userID int) error
 }
 
-type UserRepository struct {
-	DB *pgxpool.Pool
+type UserRepositoryImpl struct {
+	DB *sql.DB
 }
 
-func NewUserRepository(db *pgxpool.Pool) Repository {
-	return UserRepository{
+func NewUserRepository(db *sql.DB) UserRepository {
+	return UserRepositoryImpl{
 		DB: db,
 	}
 }
 
-func (ur UserRepository) Get(ctx context.Context, userID int) (*entity.User, error) {
-	r := ur.DB.QueryRow(ctx, "SELECT id, email, username, password, profile_picture FROM users WHERE id = $1", userID)
+func (ur UserRepositoryImpl) Get(ctx context.Context, userID int) (*entity.User, error) {
+	r := ur.DB.QueryRowContext(ctx, "SELECT id, email, username, password, profile_picture FROM users WHERE id = $1", userID)
 	user := &entity.User{}
 
 	err := r.Scan(&user.ID, &user.Email, &user.Username, &user.Password, &user.ProfilePicture)
@@ -35,13 +38,14 @@ func (ur UserRepository) Get(ctx context.Context, userID int) (*entity.User, err
 	return user, nil
 }
 
-func (ur UserRepository) Create(ctx context.Context, user *UserCreateRequest) (*UserCreateResponse, error) {
-	_, err := ur.DB.Exec(ctx, "INSERT INTO users (email, username, password, profile_picture) VALUES ($1, $2, $3, $4)", user.Email, user.Username, user.Password, user.ProfilePicture)
-	if err != nil {
-		return nil, err
+func (ur UserRepositoryImpl) Create(ctx context.Context, user *UserCreateRequest) (*UserCreateResponse, error) {
+	_, err := ur.DB.ExecContext(ctx, "INSERT INTO users (email, username, password, profile_picture) VALUES ($1, $2, $3, $4)", user.Email, user.Username, user.Password, user.ProfilePicture)
+	if err, ok := err.(*pq.Error); ok {
+		log.Fatal(err.Code, " ", err.Constraint, " ", err.Column, " ", err.Detail)
+		return nil, errors.New("unique constraint error")
 	}
 
-	r := ur.DB.QueryRow(ctx, "SELECT id FROM users WHERE email = $1", user.Email)
+	r := ur.DB.QueryRowContext(ctx, "SELECT id FROM users WHERE email = $1", user.Email)
 	userResponse := &UserCreateResponse{}
 	r.Scan(&userResponse.ID)
 	userResponse.Email = user.Email
@@ -51,13 +55,13 @@ func (ur UserRepository) Create(ctx context.Context, user *UserCreateRequest) (*
 	return userResponse, nil
 }
 
-func (ur UserRepository) Update(ctx context.Context, user *UserUpdateRequest) (*UserUpdateResponse, error) {
+func (ur UserRepositoryImpl) Update(ctx context.Context, user *UserUpdateRequest) (*UserUpdateResponse, error) {
 	SQL := "UPDATE users SET email = $1, username = $2, password = $3, profile_picture = $4 WHERE id = $5"
-	_, err := ur.DB.Exec(ctx, SQL, user.Email, user.Username, user.Password, user.ProfilePicture, user.ID)
+	_, err := ur.DB.ExecContext(ctx, SQL, user.Email, user.Username, user.Password, user.ProfilePicture, user.ID)
 	if err != nil {
 		return nil, err
 	}
-	r := ur.DB.QueryRow(ctx, "SELECT id FROM users WHERE id = $1", user.ID)
+	r := ur.DB.QueryRowContext(ctx, "SELECT id FROM users WHERE id = $1", user.ID)
 	userResponse := &UserUpdateResponse{}
 	r.Scan(&userResponse.ID)
 	userResponse.Email = user.Email
@@ -67,8 +71,8 @@ func (ur UserRepository) Update(ctx context.Context, user *UserUpdateRequest) (*
 	return userResponse, nil
 }
 
-func (ur UserRepository) Delete(ctx context.Context, userID int) error {
-	_, err := ur.DB.Exec(ctx, "DELETE FROM users WHERE id = $i", userID)
+func (ur UserRepositoryImpl) Delete(ctx context.Context, userID int) error {
+	_, err := ur.DB.ExecContext(ctx, "DELETE FROM users WHERE id = $i", userID)
 	if err != nil {
 		return err
 	}
